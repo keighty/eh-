5eb57f8a160ac88a9b2471803a9ef595d4cc3b6c
keighty <k80.leonard@gmail.com>
2013-08-08 13:35:30 -0700

    adds nodes for parser

--- lib/nodes.rb
+++ lib/nodes.rb
@@ -9,10 +9,7 @@ class Nodes
     self
   end
 
-  # This method is the "interpreter" part of our language. All nodes know how to eval
-  # itself and returns the result of its evaluation by implementing the "eval" method.
-  # The "context" variable is the environment in which the node is evaluated (local
-  # variables, current class, etc.).
+  # This method is the "interpreter" part of our language. All nodes know how to eval itself and returns the result of its evaluation by implementing the "eval" method. The "context" variable is the environment in which the node is evaluated (local variables, current class, etc.).
   def eval(context)
     # The last value evaluated in a method is the return value.
     @nodes.map { |node| node.eval(context) }.last
