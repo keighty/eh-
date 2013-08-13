26c67212b7c10aa55011e4d1ef93d08e01488c91
keighty <k80.leonard@gmail.com>
2013-08-13 13:44:38 -0700

    adds can bin

---
+++ bin/can.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# The Canadian language!
+#
+# usage:
+#   ./can example.can      # to eval a file
+#   ./can                  # to start the REPL
+#
+# on Windows run with: ruby awesome [options]
+require "parser"
+require "runtime"
+require "readline"
+
+def eval(code)
+  Parser.new.parse(code).eval(Runtime).ruby_value
+end
+
+# If a file is given we eval it.
+if file = ARGV.first
+  eval File.read(file)
+
+# Start the REPL, read-eval-print-loop, or interactive interpreter
+else
+  puts "Awesome REPL, CTRL+C to quit"
+  loop do
+    line = Readline::readline(">> ")
+    Readline::HISTORY.push(line)
+    puts "=> #{eval(line).inspect}"
+  end
+
+end
--- can.rb
+++ can.rb
@@ -1,1 +1,2 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'bin/can'
