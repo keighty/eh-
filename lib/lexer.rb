0ec13ed025ee70d37051449081ce446f8068b971
keighty <k80.leonard@gmail.com>
2013-08-15 11:08:12 -0700

    modified fixtures to include eh?

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -6,13 +6,13 @@ class Lexer
     i = 0
     tokens = []
     current_indent = 0
-    indent_stack = []
 
     while i < code.size
       chunk = code[i..-1]
 
       # checks keywords
       if identifier = chunk[/\A(eh\?)/, 1]
+        tokens << ['}', '}']
         i += identifier.size
 
       elsif identifier = chunk[/\A([a-z]\w*)/, 1]
@@ -38,30 +38,9 @@ class Lexer
         tokens << [:NUMBER, number.to_i]
         i += number.size
 
-      # checks for block indentation
-      elsif indent = chunk[/\A\:\n( +)/m, 1]
-        if indent.size <= current_indent
-          raise "Bad indent level, got #{indent.size} indents, " +
-                "expected > #{current_indent}"
-        end
-        current_indent = indent.size
-        indent_stack.push(current_indent)
-        tokens << [:INDENT, indent.size]
-        i += indent.size + 2
-
-      # check for other ending
-      elsif indent = chunk[/\A\n( *)/m, 1]
-        if indent.size == current_indent
-          tokens << [:NEWLINE, "\n"]
-        elsif indent.size < current_indent
-          indent_stack.pop
-          current_indent = indent_stack.first || 0
-          tokens << [:DEDENT, indent.size]
-          tokens << [:NEWLINE, "\n"]
-        else
-          raise "Missing ':'"
-        end
-        i += indent.size + 1
+      elsif chunk.match(/\A\n+/)
+        tokens << [:NEWLINE, "\n"]
+        i += 1
 
       elsif chunk.match(/\A /)
         i += 1
