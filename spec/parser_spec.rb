45f2b6445d37eaa5496a334d9b5d32876cb7b630
keighty <k80.leonard@gmail.com>
2013-09-05 09:45:37 -0700

    adds to class definitions for avoiding empty methods

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,43 +6,41 @@ describe Parser do
 
 class_a_test = <<-EOS
 a Car:
-  "hockey"
+  pass
 eh?
 EOS
 
 class_an_test = <<-EOS
 an Automobile:
+  pass
 eh?
 EOS
 
-  before do
-    block = lambda { }
-    @nodes = Nodes.new([
-      ClassNode.new("Car", "hockey")
-    ])
-    # p @nodes
-  end
-
   it "should parse A class" do
     # p Lexer.new.tokenize(class_a_test)
-    @nodes.should == Parser.new.parse(class_a_test)
+    # p Parser.new.parse(class_a_test)
+    expect(Parser.new.parse(class_a_test).inspect).to match '<Nodes:'
+    expect(Parser.new.parse(class_a_test).inspect).to match 'CallNode'
+    expect(Parser.new.parse(class_a_test).inspect).to match '@arguments'
+
   end
 
-  xit "should parse AN class" do
-    nodes.should == Parser.new.parse(class_an_test)
+  it "should parse AN class" do
+    # p Parser.new.parse(class_an_test)
+    expect(Parser.new.parse(class_an_test).inspect).to match '<Nodes:'
+    expect(Parser.new.parse(class_an_test).inspect).to match 'CallNode'
+    expect(Parser.new.parse(class_an_test).inspect).to match '@arguments'
+
   end
 
-  xit 'should parse a class definition' do
-    @tokens = Lexer.new.tokenize(BASIC_CLASS)
-    p @tokens
-    p Parser.new.parse(BASIC_CLASS)
+  it 'should parse a class definition' do
+    # @tokens = Lexer.new.tokenize(BASIC_CLASS)
+    # p @tokens
+    parser = Parser.new.parse(BASIC_CLASS)
 
     x = parser.instance_variable_get(:@nodes)
 
-    expect(x.first.class).to be Nodes
+    expect(x.first.class).to eq ClassNode
 
-    expect(parser.instance_variable_get(:@nodes).first.instance_variable_get(:@nodes).first.instance_variable_get(:@nodes).first.class).to be IfNode
-
-    expect(parser.instance_variable_get(:@nodes).first.instance_variable_get(:@nodes).first.instance_variable_get(:@nodes).first.instance_variable_get(:@condition).class).to be LiteralNode
   end
 end
