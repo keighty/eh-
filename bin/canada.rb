3f1a476e4ebc7f69fbe08f7c4f6dd2ef8bb83c27
keighty <k80.leonard@gmail.com>
2014-01-04 10:55:04 -0800

    Add website.

--- bin/canada.rb
+++
@@ -1,5 +0,0 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require "parser"
-require "runtime"
-require "lexer"
-require "readline"
--- spec/runtime_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe Runtime do
-  it 'should run a class' do
-    nodes = Parser.new.parse(BASIC_CLASS)
-    nodes.eval(Runtime)
-  end
-end
-
